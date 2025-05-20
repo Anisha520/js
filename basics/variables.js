18412f1c208299820144965d69b764b5a15ccafb
Anisha <151038639+Anisha520@users.noreply.github.com>
2025-05-20 12:36:12 +0000

    datatypes

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const AccountId = 5520
+let AccountEmail = "tannumandale19@gmail.com"
+var AccountPassword = "tannu"
+AccountCity = "Nagpur";
+let Accountstate;
+
+//accountId 2//not allowed
+
+/*AccountEmail = "s@udgy.com"
+AccountPassword = "jkcbkc"
+AccountId = " jaipur"
+console.log("AccountId");*/
+
+/* prefer not to use var  
+because of issue in log scope and functional scope
+*/
+console.table([AccountEmail, AccountId, AccountPassword, AccountCity, Accountstate])
