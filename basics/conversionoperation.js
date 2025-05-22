6ae6329dbfcea358a25f3702f18da4ad2357f848
Anisha <151038639+Anisha520@users.noreply.github.com>
2025-05-22 12:15:47 +0000

    coversionoperation

---
+++ basics/conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "anisha"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInnumber = Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+//"33"=> 33
+//"33abc"=>NAN(not a number)
+// true=> 1; false=>0
+
+//let isLoggedIn = "anisha"
+
+//let BooleanIsLoggedIn = Boolean(isLoggedIn)
+//console.Log(BooleanIsLoggedIn);
+
+
+//"" => false
+//"anisha" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
