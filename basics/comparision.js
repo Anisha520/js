f0e4e96c76b4873e89782da7181cd9fef2e6a7fb
Anisha <151038639+Anisha520@users.noreply.github.com>
2025-05-24 16:39:16 +0000

    changes f datatypes and comparision

---
+++ basics/comparision.js
@@ -0,0 +1,31 @@
+/*console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log(2 <= 1);*/
+
+//console.log(null <= 0);  //true
+//console.log(null >= 0);  //true
+//console.log(null > 0);  //false
+//console.log(null == 0);   ///false
+
+//comparision and equality are different
+
+//if we check it in undefined  it will give false value 
+
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+//console.log(undefined == 0);
+
+//strict check   //=== triple equal to ..it checks strictly and also checks datatypes let us see an example
+
+//console.log("2" == 2); //double equal se check krege to hojayga kyuki yaha conversion hone lagt h... //output:true
+
+//console.log("2" === 2);//yaha nahi hoga kyuki ye datatype check krta h and first value string h and second  number h... //output : false
+
+
+console.log("2" > 1); //true
+console.log("02" > 1);  //true
+console.log("2" < 0);  //false
+console.log("02" > 1); //true
---
+++ basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// 1.primitive :
+//7 types: strings(""),number,boolean(true,false),null(empty), symbol(value unique banane k liye use hota hai),bigint(scientific values big values jo handle nhi hoti wo bigint me daldete h)
+
+// javascript is dynamic type
+
+/*const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false //false
+const outsideTemp = null //null
+let userEmail;  //undefined
+console.log(score);*/
+
+
+//symbol:
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(anotherId === id); //false
+
+const bigNumber = 76863846380448n  //last me n dalne par bigint /bignumber aajata h
+
+
+//Reference type(non-primitive)
+//~ basics types
+//array,objects,functions
+
+const cookie = ["bsf", "bruhh", "gp", "ET"]  //array
+
+let myobj = {
+  name: "tannu",  //datatype kuch bhi hoskta h string,number,boolean....
+  age: 21,
+
+} //object
+
+const myfunction = function () {
+  console.log("Anisha this side");
+}
+
+//console.log(typeof ...) //datatype pata krme k liye data likho typeof k bad..
